4eadc15bdd6b4750c0e5ae31af0aae2c67190e09
Ray Tiley <raytiley@gmail.com>
2024-05-09 13:30:11 -0400

    fix some issues with widgets and titles

--- theme/functions.php
+++ theme/functions.php
@@ -132,25 +132,6 @@ if ( ! function_exists( 'cablecast_setup' ) ) :
 endif;
 add_action( 'after_setup_theme', 'cablecast_setup' );
 
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function cablecast_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => __( 'Footer', 'cablecast' ),
-			'id'            => 'sidebar-1',
-			'description'   => __( 'Add widgets here to appear in your footer.', 'cablecast' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'cablecast_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
