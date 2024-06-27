6311418eb681fed201e8f8872d202449174fe4b0
amanda-gerold <61241785+amanda-gerold@users.noreply.github.com>
2024-06-27 15:21:31 -0500

    Semi-final Design tweaks
    Add some styling to fix mobile views. Styled the restricted content message and the forms for registration. Added a link to register to the login page. Styled the schedule.

--- theme/archive-cablecast_channel.php
+++ theme/archive-cablecast_channel.php
@@ -10,7 +10,7 @@
         <div class="channel-archive-item">
             <h3 class="channel-archive-titles"><?php the_title(); ?></h3>
           <div class="channel-archive-thumbnail"><?php echo the_post_thumbnail(); ?></div>
-          <div class="pt-3"><a href="<?php echo $permalink; ?>watch" class="!text-brand-accent hover:underline">View Channel</a> | <a href="<?php echo $permalink; ?>schedule" class="!text-brand-accent hover:underline">View Schedule</a></div>
+          <div class="pt-3 pb-3"><a href="<?php echo $permalink; ?>watch" class="!text-brand-accent hover:underline">View Channel</a> | <a href="<?php echo $permalink; ?>schedule" class="!text-brand-accent hover:underline">View Schedule</a></div>
           <div><?php the_content(); ?></div>
         </div>
         
